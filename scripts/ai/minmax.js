b142360db34430c236a5e2ed2a8a19d45bfc150a
Patryk (PsychoX) Ludwikowski <patryk.ludwikowski.7@gmail.com>
2022-06-02 21:07:24 +0200

    Cache generated min-max tree to avoid lag

--- scripts/ai/minmax.js
+++ scripts/ai/minmax.js
@@ -1,14 +1,22 @@
 
+async function sha1(string) {
+	const sourceBytes = new TextEncoder().encode(string);
+	const digest = await (crypto.subtle || crypto.webcrypto.subtle).digest("SHA-1", sourceBytes);
+	const resultBytes = [...new Uint8Array(digest)];
+	return resultBytes.map(x => x.toString(16).padStart(2, '0')).join("");
+}
+
 async function delay(milliseconds) {
 	return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+async function nextFrame() {
+	return new Promise(resolve => requestAnimationFrame(resolve));
 }
 
 /**
  * @callback gameAction
  * @param {Game} game Game to affect.
  */
-
-let minMaxNodeCount = 0;
 
 class MinMaxNode {
 	/**
@@ -26,7 +34,6 @@ class MinMaxNode {
 		/** @type {number} */ this.score = -Infinity;
 
 		this.applyTo(this.game);
-		minMaxNodeCount += 1;
 	}
 
 	/**
@@ -45,16 +52,16 @@ class MinMaxNode {
 	}
 
 	isMyTurn() {
-		return this.game.currentPlayer == this.player
+		return this.game.currentPlayer == this.player;
 	}
 
-	prepare() {
+	async prepare() {
 		const aggregate = this.player == this.game.currentPlayer ? Math.max : Math.min;
 		switch (this.game.phase) {
 			case 'placing': 
 				for (const {x, y} of this.game.placePossibilitiesGenerator()) {
 					const child = new MinMaxNode(this.player, this.game.clone(), 'place', x, y);
-					child.prepare();
+					await child.prepare();
 					this.children.push(child);
 				}
 				this.score = aggregate.apply(null, this.children.map(c => c.score));
@@ -62,7 +69,7 @@ class MinMaxNode {
 			case 'moving': 
 				for (const {sx, sy, tx, ty} of this.game.movePossibilitiesForSymbolGenerator(this.game.currentPlayerSymbol)) {
 					const child = new MinMaxNode(this.player, this.game.clone(), 'move', sx, sy, tx, ty);
-					child.prepare();
+					await child.prepare();
 					this.children.push(child);
 				}
 				this.score = aggregate.apply(null, this.children.map(c => c.score));
@@ -79,7 +86,7 @@ class MinMaxNode {
 				this.score = -1;
 				break;
 		}
-		return this;
+		this.game = null;
 	}
 
 	countNodes() {
@@ -92,6 +99,25 @@ class MinMaxNode {
 }
 
 class MinMaxStrategy extends GameStrategy {
+	static _cachedRoot = null;
+	static _cachedRootHash = null;
+
+	/**
+	 * @param {number} player 
+	 * @param {Game} game 
+	 */
+	static async prepareRoot(player, game) {
+		const hash = await sha1(JSON.stringify(game.settings) + player);
+		if (MinMaxStrategy._cachedRootHash == hash) {
+			return MinMaxStrategy._cachedRoot;
+		}
+
+		MinMaxStrategy._cachedRoot = new MinMaxNode(player, game.clone());
+		await MinMaxStrategy._cachedRoot.prepare();
+		MinMaxStrategy._cachedRootHash = hash;
+		return MinMaxStrategy._cachedRoot;
+	}
+
 	constructor() {
 		super();
 		/** @type {MinMaxNode[]|null} */
@@ -117,21 +143,19 @@ class MinMaxStrategy extends GameStrategy {
 	}
 
 	async start() {
-		// Minimal delay is required
-		await delay(333);
+		// Minimal delay is required to observe game in first phase (as 'start' event is fired before 'phase')
+		await nextFrame();
 
-		minMaxNodeCount = 0;
-
-		// TODO: add ref to node, prevent exploring already solved states (add `game.hash()`)
-		this.possibilities = new MinMaxNode(this.player, this.game.clone()).prepare().children;
+		const root = await MinMaxStrategy.prepareRoot(this.player, this.game);
+		this.possibilities = root.children;
 	}
 
 	/**
 	 * Performs next action.
 	 */
 	async next() {
-		// Minimal delay is required
-		await delay(333);
+		// Wait for ready
+		await this.readyPromise;
 		console.log('nodes: ' + this.countNodes());
 
 		this.debugPrintPossibilities();
@@ -150,7 +174,7 @@ class MinMaxStrategy extends GameStrategy {
 	attach(game, player = 1) {
 		super.attach(game, player);
 		this.game.addEventListener('start', this.startListener = event => {
-			this.start();
+			this.readyPromise = this.start();
 		});
 		this.game.addEventListener('next', this.nextListener = event => {
 			if (this.player == event.player) {
